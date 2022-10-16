18f058fe22b06f012db128e7f91f1d2eeb7dc09f
TomekA <tomek.artecki@aexol.com>
2022-10-16 17:49:32 +0200

    adding face-recogn-app to repository (splitting repo on 2 apps - one for learning model and another for using it)

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# face-recogn-system
+## Face recognition system
 
-An app for training machine learning model for detecting faces of celebrities
+# celebrity-face-learn-app
 
-needed dataset:
+A python app for training machine learning model for detecting faces of celebrities.
+
+Needed dataset:
 https://www.kaggle.com/datasets/hereisburak/pins-face-recognition
 
 can be downloaded with:
 kaggle datasets download -d hereisburak/pins-face-recognition
+
+Run app by running:
+cd celebrity-face-learn-app
+docker-compose up
+
+# face-recogn-app
+
+A python web app for detecting faces which uses cv2 for face detection and model developed in celebrity-face-learn-app.
+
+How to run:
+
+1. Generate model files using celebrity-face-learn-app(or placing them there manually):
+   celebrity-face-learn-app/Model/FaceRecogn.h5 (file with model)
+   celebrity-face-learn-app/Model/classes.txt (file with labels(celebrities names) for indexes extracted from model)
+
+2. Run file:
+   cd face-recogn-app && ./face-recogn-app.py
---
+++ face-recogn-app/face-recogn-app.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, Response
+import cv2
+import os
+from keras import models
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+#Initialize the Flask app
+app = Flask(__name__)
+
+#Load model
+model = models.load_model('../celebrity-face-learn-app/Model/FaceRecogn.h5')
+classes = np.genfromtxt('../celebrity-face-learn-app/Model/classes.txt', dtype='str', delimiter='\n')
+print('\nClasses detected:\n', classes)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+camera = cv2.VideoCapture(0)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        ##
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.6, minNeighbors=3)
+        for (x, y, w, h) in faces:
+            #print('Person detected!',x, y, w, h)
+            # roi_gray = gray[y:y+h, x:x+w]   #region of interest
+            roi_color = frame[y:y+h, x:x+w]
+            #cv2.imwrite(img_item, roi_gray)
+            
+            img_array = np.array(roi_color)
+            
+            #Resize dimensions to match tha input to model
+            img_array = np.resize(img_array, (256, 256, 3)) 
+
+            from keras.applications.inception_resnet_v2 import preprocess_input
+            # path = random_face()
+
+            # img= image.load_img(path, target_size=(256,256,3))
+            # plt.imshow(img)
+            # plt.axis("off")
+            # i = image.img_to_array(img)
+            i=preprocess_input(img_array)
+            input_arr = np.array([i])
+
+            pred= np.argmax(model.predict(input_arr))
+
+            #using model for name predicting
+            name = classes[pred]
+            # print(classes[pred])
+            
+            #draw rectangle on screen
+            color = (0, 0, 255) #BGR
+            stroke = 2
+            end_cord_x = x + w
+            end_cord_y = y + h
+            cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+            
+        ##
+        
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+                   
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+if __name__ == "__main__":
+    app.run()
+    
+    
+#pip install markupsafe==2.0.1
+#pip install Werkzeug==2.0.3
