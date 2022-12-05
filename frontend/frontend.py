3caa9b41c760b732a60ba404b10a00cf39bbe95d
DTomArt <dtomart@gmail.com>
2022-12-05 22:58:02 +0000

    k8s config fix, changing frontend -> now it accepts IP address of a pod in a form to which it connects

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, request, Response
 import time
 import logging
 import requests
+import re
 
 #Initialize the Flask app
 app = Flask(__name__)
@@ -17,17 +18,25 @@ def index():
 # @app.route('/redir')
 # def redir():
 #     return redirect(url_for('camera'))
-    
-@app.route('/camera')
-def camera():
-    return render_template('camera.html')
 
-@app.route('/video_feed')
-def video_feed():
-    res=requests.get('http://webapp:5000/camera', stream=True)
+
+@app.route('/camera', methods=['POST'])
+def camera():
+    pod_ip = request.form['pod_IP']
+    return render_template('camera.html', pod_ip=pod_ip)
+
+@app.route('/<pod_ip>/video_feed')
+def video_feed(pod_ip):
+    # check if ip is valid
+    match = re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", pod_ip)
+    if bool(match) is False:
+        print("IP address {} is not valid".format(pod_ip))
+        return Response("bad_input", status=401, mimetype='application/json')
+    
+    res=requests.get('http://' + pod_ip +':5000/camera', stream=True)
     print(res)
     return Response(res.iter_content(chunk_size=10*1024), mimetype='multipart/x-mixed-replace; boundary=frame')
-    # return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
     
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5080)
