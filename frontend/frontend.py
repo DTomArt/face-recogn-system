ce50a3521b0ebafd326444ddb033b7678217742e
DTomArt <dtomart@gmail.com>
2022-12-16 20:06:20 +0000

    face-recogn-app: error handling change8

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -27,17 +27,16 @@ def video_feed(pod_ip):
     # if bool(match) is False:
     #     print("IP address {} is not valid".format(pod_ip))
     #     return Response("bad_input", status=401, mimetype='application/json')
-    try:
-        res=requests.get('http://webapp-svc.default:5000/camera', stream=True)
-    except:
+
+    res=requests.get('http://webapp-svc.default:5000/camera', stream=True)
+
+    if not res:
         err=imageio.imread('error.png')
         return send_file(err, mimetype='image/gif')
+        
     return Response(res.iter_content(chunk_size=10*1024), mimetype='multipart/x-mixed-replace; boundary=frame')
 
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5080)
-    
-    
-#pip install markupsafe==2.0.1
-#pip install Werkzeug==2.0.3
+
