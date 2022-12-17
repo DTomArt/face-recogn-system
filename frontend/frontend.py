ea0a0bb8960a95aa988a9ff5e58bfc574b850ec1
DTomArt <dtomart@gmail.com>
2022-12-17 14:29:15 +0000

    cleaning up frontend2

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -22,6 +22,8 @@ def camera():
 @app.route('/video_feed')
 def video_feed():
     res=requests.get('http://webapp-svc.default:5000/camera', stream=True)
+    if not res:
+        print('nie dziala')
     return Response(res.iter_content(chunk_size=10*1024), mimetype='multipart/x-mixed-replace; boundary=frame')
     
 if __name__ == "__main__":
