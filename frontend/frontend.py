90758e50897d3f7e42e9cc8523c1dab3dd82a97e
DTomArt <dtomart@gmail.com>
2022-12-17 14:03:25 +0000

    cleaning up frontend - deleting comments and ip to insert

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -17,21 +17,12 @@ def index():
 
 @app.route('/camera', methods=['POST'])
 def camera():
-    pod_ip = request.form['pod_ip']
     return render_template('camera.html', pod_ip=pod_ip)
 
-@app.route('/<pod_ip>/video_feed')
-def video_feed(pod_ip):
-    # check if ip is valid
-    # match = re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):(6553[0-5]|655[0-2][0-9]|65[0-4][0-9][0-9]|6[0-4][0-9][0-9][0-9][0-9]|[1-5](\d){4}|[1-9](\d){0,3})$", pod_ip)
-    # if bool(match) is False:
-    #     print("IP address {} is not valid".format(pod_ip))
-    #     return Response("bad_input", status=401, mimetype='application/json')
-
+@app.route('/video_feed')
+def video_feed():
     res=requests.get('http://webapp-svc.default:5000/camera', stream=True)
     return Response(res.iter_content(chunk_size=10*1024), mimetype='multipart/x-mixed-replace; boundary=frame')
-
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5080)
-
