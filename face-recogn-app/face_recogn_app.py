24b39a484a1d881441bfbdf4d8cccfef9d77bdcd
DTomArt <dtomart@gmail.com>
2023-01-04 10:40:45 +0000

    backend - run app fix

--- face-recogn-app/face_recogn_app.py
+++ face-recogn-app/face_recogn_app.py
@@ -114,4 +114,4 @@ def index():
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=5000)
-    socketio.run(host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000)
